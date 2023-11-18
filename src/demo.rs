9cf58d75c19cea45b6f9b444d364f8bcc2dcea74
Tomasz Dłuski <mr.toumash@gmail.com>
2023-11-18 16:05:15 +0100

    working code

--- src/demo.rs
+++ src/demo.rs
@@ -16,8 +16,8 @@ type Message = Vec<u8>;
 type Name = String;
 
 struct MessageBroker {
-    queues: HashMap<String, Queue>,
-    exchanges: HashMap<String, Exchange>,
+    queues: HashMap<String, Arc<Queue>>,
+    exchanges: HashMap<String, Arc<Mutex<Exchange>>>,
 }
 
 impl MessageBroker {
@@ -30,33 +30,36 @@ impl MessageBroker {
 
     fn declare_queue(&mut self, queue: Name) {
         let q = Queue::new();
-        q.run();
-        self.queues.insert(queue, q);
+        self.queues.insert(queue, q.into());
     }
 
     fn declare_exchange(&mut self, exchange: Name) {
         let name_copy = exchange.clone();
-        self.exchanges.insert(exchange, Exchange::new(name_copy));
+        let e = Exchange::new(name_copy);
+        let v = Mutex::from(e).into();
+        self.exchanges.insert(exchange, v);
     }
 
-    fn bind_queue(&mut self, exchange_name: Name, queue_name: Name) {
+    fn bind_queue(&mut self, exchange_name: Name, queue_name: Name) -> Option<SyncSender<Message>> {
         let queue = self.queues.get(&queue_name);
         let exchange = self.exchanges.get(&exchange_name);
 
         match (queue, exchange) {
             (Some(q), Some(e)) => {
                 let sender = q.channel.0.clone();
-                e.bound_channel = Some(sender);
+                e.lock().unwrap().bound_channel = Some(sender);
+                return Some(q.channel.0.clone());
             }
             (_, _) => {
                 print!("no queue/exchange with this name 3");
+                return None;
             }
-        }
+        };
     }
 
     fn basic_publish(&self, exchange_name: &Name, message: Message) {
         match self.exchanges.get(exchange_name) {
-            Some(e) => e.basic_publish(message),
+            Some(e) => e.lock().unwrap().basic_publish(message),
             None => println!("No exchange with this name"),
         }
     }
@@ -68,14 +71,12 @@ impl MessageBroker {
         }
     }
 
-    fn run(&self) {
-        for key in self.queues.keys() {
-            let queue = &self.queues.get(key).unwrap();
-            thread::spawn(|| {
-                queue.run();
-            });
-        }
-    }
+    // fn run(&self) {
+    //     for key in self.queues.keys() {
+    //         let queue = &self.queues.get(key).unwrap();
+    //         queue.run();
+    //     }
+    // }
 }
 
 struct Exchange {
@@ -94,39 +95,35 @@ impl Exchange {
         self.bound_channel = Some(sender);
     }
 
-    fn basic_publish(&mut self, message: Message) {
-        match self.bound_channel {
-            Some(c) => {
-                c.send(message);
-            }
-            None => todo!(),
-        }
+    fn basic_publish(&self, message: Message) {
+        self.bound_channel.as_ref().unwrap().send(message);
     }
 }
 
 struct Queue {
     queue: Arc<Mutex<VecDeque<Message>>>,
-    channel: (SyncSender<Message>, Receiver<Message>),
+    channel: (SyncSender<Message>, Arc<Receiver<Message>>),
 }
 
 impl Queue {
     fn new() -> Self {
+        let c = sync_channel(1000);
         Queue {
             queue: Arc::new(Mutex::new(VecDeque::new())),
-            channel: sync_channel(1000),
+            channel: (c.0, c.1.into()),
         }
     }
 
-    fn run(&self) {
-        let mut moved_channel = self.channel.1;
-        loop {
-            if let message = moved_channel.recv().unwrap() {
-                self.queue.lock().unwrap().push_back(message);
-                // on_message_callback(message);
-            }
-            thread::sleep(Duration::from_micros(100))
-        }
-    }
+    // fn run(&self) {
+    //     let mut moved_channel = self.channel.1;
+    //     loop {
+    //         if let message = moved_channel.recv().unwrap() {
+    //             self.queue.lock().unwrap().push_back(message);
+    //             // on_message_callback(message);
+    //         }
+    //         thread::sleep(Duration::from_micros(100))
+    //     }
+    // }
 
     fn push(&self, message: Message) {
         let mut queue = self.queue.lock().unwrap();
@@ -134,43 +131,41 @@ impl Queue {
     }
 
     fn receive(&self, on_message_callback: CallbackType) {
-        thread::spawn(move || {
-            loop {
-                let mut queue_write = self.queue.lock().unwrap();
-                if let Some(message) = queue_write.pop_front() {
-                    drop(queue_write); // Explicitly drop lock before callback call to prevent dead-lock in case of time consuming callback
-                    on_message_callback(message);
-                }
-                thread::sleep(Duration::from_micros(100))
+        let mutex = &self.queue;
+        loop {
+            let msg = self.channel.1.recv().unwrap();
+            self.queue.lock().unwrap().push_back(msg);
+
+            let mut queue_write = mutex.lock().unwrap();
+            if let Some(message) = queue_write.pop_front() {
+                drop(queue_write); // Explicitly drop lock before callback call to prevent dead-lock in case of time consuming callback
+                on_message_callback(message.clone());
             }
-        });
+            thread::sleep(Duration::from_micros(100))
+        }
     }
 }
 
 fn on_receive(message: Message) {
-    print!("Message: {:?}", message);
+    println!("Message: {:?}", String::from_utf8(message));
 }
 
 fn main() {
     let mut broker = MessageBroker::new();
     broker.declare_queue(String::from("queue"));
     broker.declare_exchange(String::from("exchange"));
-    broker.bind_queue(String::from("exchange"), String::from("queue"));
+    let e = broker
+        .bind_queue(String::from("exchange"), String::from("queue"))
+        .unwrap();
 
-    broker.basic_consume(&String::from("exchange"), on_receive);
-
-    let mut threads = Vec::new();
     // Producer thread
     for x in 1..1000 {
-        threads.push(thread::spawn(move || {
-            let message = b"Hello, world!".to_vec();
-            broker.basic_publish(&String::from("main"), message);
-            println!("Message pushed to queue");
-        }));
+        let message = b"Hello, world!".to_vec();
+        e.send(message);
+        println!("Message pushed to queue");
     }
-    broker.run();
 
-    for p in threads {
-        p.join().unwrap();
-    }
+    broker.basic_consume(&String::from("queue"), |message| {
+        println!("Message: {:?}", String::from_utf8(message));
+    });
 }
